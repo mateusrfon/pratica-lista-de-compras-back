17b638923ba92449ef607827ddeac5de3d022d40
Mateus Ribeiro <mateusrfon@gmail.com>
2021-07-12 15:36:50 -0300

    post and get items

--- src/app.js
+++ src/app.js
@@ -7,4 +7,29 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.post('/list', async (req,res) => {
+    try {
+        const { text } = req.body;
+        await connection.query(`
+            INSERT INTO items
+            (text) VALUES ($1)
+        `, [text]);
+        res.sendStatus(201);
+    } catch(err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+});
+
+app.get('/list', async (req,res) => {
+    try {
+        const request = await connection.query('SELECT * FROM items');
+        const items = request.rows;
+        res.send(items);
+    } catch(err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+});
+
 export default app;
--- src/database.js
+++ src/database.js
@@ -3,11 +3,11 @@ import pg from "pg";
 const { Pool } = pg;
 
 const connection = new Pool({
-  user: "bootcamp_role",
-  password: "senha_super_hiper_ultra_secreta_do_role_do_bootcamp",
+  user: "postgres",
+  password: "123456",
   host: "localhost",
   port: 5432,
-  database: "my_shopping_list"
+  database: "lista_de_compras"
 });
 
 export default connection;
